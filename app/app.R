e7049fbee28070d460d0fafe32906f2dc17e96db
chris.j.van.diemen@gmail.com <chris.j.van.diemen@gmail.com>
2018-04-01 22:46:37 +0000

    changed folder names

--- app/app.R
+++ app/app.R
@@ -1,50 +1,120 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(leaflet)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+city = 'Amsterdam'
+
+
+
+mtcars <- mtcars[1:5,]
+
+header <- dashboardHeader(title = city,titleWidth = 300,disable = TRUE)
+
+sidebar <- dashboardSidebar( actionButton("recalc", "New points"),disable = TRUE)
+
+body <- dashboardBody(fluidRow(
+  box(  leafletOutput("mymap",height = "500"), background = 'black', width = 12)),
+  
+  fluidRow(
+    box(plotOutput("plot1"),
+    background = 'black', width = 6
+    
+  )),
+                         p(),
+                         
+                         # Also add some custom CSS to make the title background area the same
+                         # color as the rest of the header.
+                         
+                         tags$head(tags$style(HTML('
+
+                                                    .content-wrapper,
+                                                    .right-side {
+                                                      background-color: #000000;
+                                                    }
+                                                    
+                                                    .leafletOutput {
+                                                        background-color:rgba(255,0,0,0.0);
+                                                    }
+
+                                                    body, label, input, button, select { 
+                                                    font-family: "Calibri";
+                                                    background-color: black;
+                                    
+                                                   /* logo */
+                                                   .skin-blue .main-header .logo {
+                                                   background-color: #000000;
+                                                   }
+                                                   
+                                                   /* logo when hovered */
+                                                   .skin-blue .main-header .logo:hover {
+                                                   background-color: #000000;
+                                                   }
+                                                   
+                                                   /* navbar (rest of the header) */
+                                                   .skin-blue .main-header .navbar {
+                                                   background-color: #000000;
+                                                   }        
+                                                   
+                                                   /* main sidebar */
+                                                   .skin-blue .main-sidebar {
+                                                   background-color: #ffffff;
+                                                   }
+                                                   
+                                                   /* active selected tab in the sidebarmenu */
+                                                   .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+                                                   background-color: #000000;
+                                                   }
+                                                   
+                                                   /* other links in the sidebarmenu */
+                                                   .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+                                                   background-color: #000000;
+                                                   color: #000000;
+                                                   }
+                                                   
+                                                   /* other links in the sidebarmenu when hovered */
+                                                   .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover{
+                                                   background-color: #000000;
+                                                   }
+                                                   /* toggle button when hovered  */                    
+                                                   .skin-blue .main-header .navbar .sidebar-toggle:hover{
+                                                   background-color: #ffffff;
+                                                   }
+                                                   ')))
+                         
+                        )
+
+dashboardPage(header, sidebar, body)
+
+
+ui <- dashboardPage( header,sidebar,body
+                     
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+
+
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+
+server <- function(input, output, session) {
+  
+  output$plot1 <- renderPlot(
+    dotchart(mtcars$mpg,labels=row.names(mtcars),cex=.7,
+             main="Gas Milage for Car Models", 
+             xlab="Miles Per Gallon",color = 'black')
+  )
+  
+  points <- eventReactive(input$recalc, {
+    cbind(rnorm(50) * 1 + 4, rnorm(50) + 52)
+  }, ignoreNULL = FALSE)
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.DarkMatter,
+                       options = providerTileOptions(noWrap = FALSE)
+      ) %>%
+      addCircleMarkers(data = points(),color = 'green')
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
