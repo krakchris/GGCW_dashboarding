00967bdec50d0b94291238f416e145dccb49b326
Chris <35135810+krakchris@users.noreply.github.com>
2018-04-09 01:16:37 +0200

    Add dropdown, remove zoomcontrol

--- app/app.R
+++ app/app.R
@@ -15,17 +15,29 @@ header <- dashboardHeader(title = city,titleWidth = 300,disable = TRUE)
 
 sidebar <- dashboardSidebar( actionButton("recalc", "New points"),disable = TRUE)
 
-body <- dashboardBody(fluidRow(
+body <- dashboardBody(
+  
+  fluidRow(selectInput("City", "City:",
+                       c("Amsterdam" = "cyl",
+                         "Houston" = "am",
+                         "Rio" = "gear"))),
+  
+  
+  fluidRow(
                       box(  leafletOutput("mymap",height = "500"), background = "black", width = 12)),
   
                         # Also add some custom CSS to make the title background area the same
                         # color as the rest of the header.
+                      
+
                        
                         tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
                         ),
                       tags$div(
                         HTML(sprintf(html, "50% 100%", "4","string", "80% 100%", "65% 100%"))
                       )
+                      
+
                          
                         )
 
@@ -49,7 +61,7 @@ server <- function(input, output, session) {
     cbind(rnorm(50) * 1 + 4, rnorm(50) + 52)
   }, ignoreNULL = FALSE)
   output$mymap <- renderLeaflet({
-    leaflet() %>%
+    leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
       addProviderTiles(providers$CartoDB.DarkMatter,
                        options = providerTileOptions(noWrap = FALSE)
       ) %>%
