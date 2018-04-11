ed22508dbdab31d920bcb20d8b75fd3e246f15c9
Chris <35135810+krakchris@users.noreply.github.com>
2018-04-12 00:30:08 +0200

    add functions and some data analysis

--- app/app.R
+++ app/app.R
@@ -5,6 +5,64 @@ library(feather)
 
 
 
+
+# read datafile
+df <- read_feather("Amsterdam_score.feather")
+
+
+# calculat mean scoring over all values
+df$mean_score  <- rowMeans(df[,c(6,7,8,10,11,12,13,14)])
+
+# create secuence based on spread of scoring values
+cut_values <- seq(min(df$mean_score)+0.1, max(df$mean_score)-0.1, length.out=5)
+cut_values_econ <- seq(min(df$Monetary)+1, max(df$Monetary)-1, length.out=5)
+
+
+
+df$name <- as.factor(df$name)
+
+######################################
+# handy functions
+#######################################
+
+
+
+
+getColor <- function(input) {
+  sapply(input, function(input) {
+    if(input < cut_values[1]) {
+      "#E50006"
+    } else if(input <= cut_values[2]) {
+      "#AB2F08"
+    } else if(input <= cut_values[3]) {
+      "#725F0A"
+    } else if(input <= cut_values[4]) {
+      "#398F0C"
+    } else {
+      "#00BF0F"
+    } })
+}
+
+getScore_econ <- function(input) {
+  sapply(input, function(input) {
+    
+    if(input < cut_values_econ[1]) {
+      as.numeric(1)
+    } else if(input <= cut_values_econ[2]) {
+      as.numeric(2)
+    } else if(input <= cut_values_econ[3]) {
+      as.numeric(3)
+    } else if(input <= cut_values_econ[4]) {
+      as.numeric(4)
+    } else {
+      as.numeric(5)
+    } })
+}
+
+#########################################
+
+
+
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
 
