f23ca5f634003977aa19a4fb4242bb73b132e9ba
Chris <35135810+krakchris@users.noreply.github.com>
2018-04-11 14:53:18 +0200

    fixed variable seq.

--- app/app.R
+++ app/app.R
@@ -1,16 +1,33 @@
+rm(list=ls())
+
+
 library(shiny)
 library(leaflet)
 library(htmltools)
 library(feather)
 
 
+# read datafile
+df <- read_feather("Amsterdam_score.feather")
+
+
+# calculat mean scoring over all values
+df$mean_score  <- rowMeans(df[,c(6,7,8,10,11,12,13,14)])
+
+# create secuence based on spread of scoring values
+cut_values <- seq(min(df$mean_score)+0.1, max(df$mean_score)-0.1, length.out=5)
+cut_values_econ <- seq(min(df$Monetary)+1, max(df$Monetary)-1, length.out=5)
+
+
+
+df$name <- as.factor(df$name)
 
 ######################################
 # handy functions
 #######################################
 
 
-df$name <- as.factor(df$name)
+
 
 getColor <- function(input) {
   sapply(input, function(input) {
@@ -31,15 +48,15 @@ getScore_econ <- function(input) {
   sapply(input, function(input) {
     
     if(input < cut_values_econ[1]) {
-      1
+      as.numeric(1)
     } else if(input <= cut_values_econ[2]) {
-      2
+      as.numeric(2)
     } else if(input <= cut_values_econ[3]) {
-      3
+      as.numeric(3)
     } else if(input <= cut_values_econ[4]) {
-      4
+      as.numeric(4)
     } else {
-      5
+      as.numeric(5)
     } })
 }
 
@@ -48,16 +65,13 @@ getScore_econ <- function(input) {
 
 city = 'amsterdam'
 
-# read datafile
-df <- read_feather("Amsterdam_score.feather")
 
 
-# calculat mean scoring over all values
-df$mean_score  <- rowMeans(df[,c(6,7,8,10,11,12,13,14)])
 
 
-# create secuence based on spread of scoring values
-cut_values <- seq(min(df$mean_score)+0.1, max(df$mean_score)-0.1, length.out=5)
+
+
+
 
 
 mean_soc <- rowMeans(df[,c(10,11)])
@@ -65,6 +79,11 @@ mean_soc <- rowMeans(df[,c(10,11)])
 mean_eco <- rowMeans(df[,c(6,7,8,12,13,14)])
 
 mean_econ<- getScore_econ(df$Monetary)
+
+
+
+
+
 
 
 
