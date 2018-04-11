89aa2f31a69f19babf4517eef42e9f34ea8659d0
Chris <35135810+krakchris@users.noreply.github.com>
2018-04-11 14:18:37 +0200

    changed dir structure

--- app/app.R
+++ app/app.R
@@ -1,354 +1,41 @@
 library(shiny)
-library(leaflet)
-library(htmltools)
-library(feather)
 library(ggmap)
+library(leaflet)
 
+ui <- shinyUI(bootstrapPage(
+  leafletOutput("map")
+))
 
-######################################
-# handy functions
-#######################################
-
-
-df$name <- as.factor(df$name)
-
-getColor <- function(input) {
-  sapply(input, function(input) {
-    if(input < cut_values[1]) {
-      "#E50006"
-    } else if(input <= cut_values[2]) {
-      "#AB2F08"
-    } else if(input <= cut_values[3]) {
-      "#725F0A"
-    } else if(input <= cut_values[4]) {
-      "#398F0C"
-    } else {
-      "#00BF0F"
-    } })
-}
-
-getScore_econ <- function(input) {
-  sapply(input, function(input) {
-    
-    if(input < cut_values_econ[1]) {
-      1
-    } else if(input <= cut_values_econ[2]) {
-      2
-    } else if(input <= cut_values_econ[3]) {
-      3
-    } else if(input <= cut_values_econ[4]) {
-      4
-    } else {
-      5
-    } })
-}
-
-#########################################
-
-
-city = 'amsterdam'
-
-# read datafile
-df <- read_feather("Amsterdam_score.feather")
-
-
-# calculat mean scoring over all values
-df$mean_score  <- rowMeans(df[,c(6,7,8,10,11,12,13,14)])
-
-
-# create secuence based on spread of scoring values
-cut_values <- seq(min(df$mean_score)+0.1, max(df$mean_score)-0.1, length.out=5)
-
-
-mean_soc <- rowMeans(df[,c(10,11)])
-
-mean_eco <- rowMeans(df[,c(6,7,8,12,13,14)])
-
-mean_econ<- getScore_econ(df$Monetary)
-
-
-
- ui <- shinyUI(bootstrapPage(theme = "bootstrap.css",
-   
-   
+server <- shinyServer(function(input, output, session) {
+  ## One alternative: store circles data?
+  ## I dont actually implement this, but you would do this in the observer as well
+  dat <- reactiveValues(circs = data.frame(lng=numeric(), lat=numeric()))
   
-   
-   
-   fluidRow( 
-
-     
-     box( theme = "bootstrap.css",selectInput("City", "",
-                        c("Amsterdam" = "Amsterdam",
-                          "Houston" = "Houston",
-                          "Rio de Janeiro" = "Rio de Janeiro",
-                          "Tokyo" = "Tokyo")), align="center", width = "100%", height= "100px", style='padding:10px; font-size: 150%; background = "black"; font-family: "Roboto";',
-         
-         
-         tags$style(type='text/css', "#button { vertical-align: middle; height: 50px; width: 100%; font-size: 40px;}")
-     )
-     
-     
-  ) ,
-   
-   
-   tags$head(HTML("<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type = 'text/css'>")),
-  
-  
-   h1(title= "hallo", style = "font-family: 'Roboto'; color: white; font-size: 64px; text-align: center;"),
-   
-  
-
-   
-  
-  
-                    box(  leafletOutput("map",height = "500"), background = "black", width = 12),
-  
-                        # Also add some custom CSS to make the title background area the same
-                        # color as the rest of the header.
-                       
-                        tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
-                        ),
-  
-  
-  
-  
-  
-  fluidRow(
-    column(align="center", width = 4, 
-  sliderInput("Social", "Social:",
-              min = 0, max = 100,
-              value = 40))  ,
-  
-  column( align="center",width = 4, 
-         sliderInput("Ecology", "Ecology:",
-                     min = 0, max = 100,
-                     value = 30)) ,
-  
-  column( align="center",width = 4, 
-         sliderInput("Economy", "Economy:",
-                     min = 0, max = 100,
-                     value = 30)) 
-  
-  
-  
-  ),
-  
- 
-  h1("Use sliders to change importance of different score groups", style = "font-family: 'Roboto'; color: white; font-size: 20px; text-align: center;"),
-  
-  
-  
-  fluidRow( 
-    column(1, align="center", tableOutput('values')
-    ),
-    column(2 ,offset = 5, align="center", tableOutput('table1')
-    )          
-                         
-                        )))
-
-
-
-###############################################################################
-# server
-###############################################################################
-
-server <- function(input, output, session) {
-   
-  acm_defaults <- function(map, x, y) 
-    addCircleMarkers(map, x, y, radius=10, color="white", fillColor="orange", 
-                     fillOpacity=0, opacity=1, weight=2, stroke=TRUE, layerId="OSM_id")
-  
-  
-  
-  
-  points <- eventReactive(input$recalc, {
-    cbind(df$X_wgs,df$Y_wgs)
-  }, ignoreNULL = FALSE)
-  
-  
+  ## Make your initial map
   output$map <- renderLeaflet({
-    leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
-      addProviderTiles(providers$CartoDB.DarkMatter,
-                       options = providerTileOptions(noWrap = FALSE)
-      ) %>%
-      addCircleMarkers(data = points(),color = getColor(
-        
-        
-        ((input$Social/100)*mean_soc)+ ((input$Economy/100)*mean_econ) + ((input$Ecology/100)*mean_eco)
-        
-        
-      ), group="locations", layerId = df$OSM_id , stroke = FALSE, fillOpacity = 1, label = htmlEscape(df$name)) 
+    leaflet() %>%
+      setView(lng = -43.1729, lat = -22.9068, zoom = 11) %>%
+      addTiles(options = providerTileOptions(noWrap = TRUE)) 
   })
   
-  
-  observeEvent(input$map_marker_click, {
+  ## Observe mouse clicks and add circles
+  observeEvent(input$map_click, {
     ## Get the click info like had been doing
+    click <- input$map_click
+    clat <- click$lat
+    clng <- click$lng
+    address <- revgeocode(c(clng,clat))
     
-    
-    print(input$map_marker_click)
-    
-    p <- input$map_marker_click
-
-
-    proxy <- leafletProxy("map")
-
-    if(p$id=="Selected"){
-      proxy %>% removeMarker(layerId="Selected")
-    } else {
-      proxy %>% setView(lng=p$lng, lat=p$lat, 12) %>% acm_defaults(p$lng, p$lat)
-    }
-    
+    ## Add the circle to the map proxy
+    ## so you dont need to re-render the whole thing
+    ## I also give the circles a group, "circles", so you can
+    ## then do something like hide all the circles with hideGroup('circles')
+    leafletProxy('map') %>% # use the proxy to save computation
+      addCircles(lng=clng, lat=clat, group='circles',
+                 weight=1, radius=100, color='black', fillColor='orange',
+                 popup=address, fillOpacity=0.5, opacity=1)
   })
   
-  
-  
-  # distribute importance over 3 categories###
-  observeEvent(input$Ecology,  {
-    
-    
-    eco_val <- input$Ecology
-    
-    econ_val <- input$Economy
-    
-    soc_val <- input$Social
-    
-    current_rest = soc_val+econ_val
-    
-  change <- (((current_rest + eco_val)-100)*-1)/2
-   
-    updateSliderInput(session = session, inputId = "Social", value = soc_val+change)
-    
-    updateSliderInput(session = session, inputId = "Economy", value = econ_val+change)
-  })
-  
-  observeEvent(input$Social,  {
-    
-    
-    eco_val <- input$Ecology
-    
-    econ_val <- input$Economy
-    
-    soc_val <- input$Social
-    
-    
-    current_rest = soc_val+econ_val
-    
-    change <- (((current_rest + eco_val)-100)*-1)/2
-    
-    updateSliderInput(session = session, inputId = "Ecology", value = eco_val+change)
-    
-    updateSliderInput(session = session, inputId = "Economy", value = econ_val+change)
-  })
-  
-  observeEvent(input$Economy,  {
-    
-    
-    eco_val <- input$Ecology
-    
-    econ_val <- input$Economy
-    
-    soc_val <- input$Social
-    
-    
-    current_rest = soc_val+econ_val
-    
-    change <- (((current_rest + eco_val)-100)*-1)/2
-    
-    updateSliderInput(session = session, inputId = "Social", value = soc_val+change)
-    
-    updateSliderInput(session = session, inputId = "Ecology", value = eco_val+change)
-    
-  })
-  
-  ############################################
+})
 
-  
-  observeEvent(input$City,  {
-    
-    df <- data.frame(
-      Name = c("Presence of amenities", "NDVI range"
-               
-               
-               
-      ),
-      
-      Value = as.integer(c(input$Social,
-                           input$Ecology),
-                         stringsAsFactors = FALSE))
-    
-    print(input$City)
-    
-    output$table1 <- renderTable(df)
-    
-  })
-    
-
- 
-  
-
-  
-  
-
-  
-
-  
-  output$table1 <- renderTable(df)
-  
-  
-  table_soc_score <- reactive({
-    
-    data.frame(
-      Name = c("Presence of amenities", "NDVI range"
-               
- 
-               
-      ),
-      
-      Value = as.integer(c(input$Social,
-                           input$Ecology),
-      stringsAsFactors = FALSE)
-      
-    )})
-  
-  
-  
-  
-  sliderValues <- reactive({
-    
-    data.frame(
-      Name = c("Social",
-               "Ecology",
-               "Economy"
-               ),
-      
-      Value = as.integer(c(input$Social,
-                             input$Ecology,
-                             input$Economy
-                             ),
-      stringsAsFactors = FALSE)
-    
- )})
-  
- 
-  output$values <- renderTable(10)
-  
-  
-
-  
-  
-  
-  
-
-  
-
-  
-  ### end of server ###
-}
-
-
-
-shinyApp(ui, server)
-
-
-
-
+shinyApp(ui=ui, server=server)
