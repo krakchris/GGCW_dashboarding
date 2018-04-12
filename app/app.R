2c107d6927608de1b0dcf718cb2ca081e1e8cd87
Chris <35135810+krakchris@users.noreply.github.com>
2018-04-13 00:32:37 +0200

    add some stuff

--- app/app.R
+++ app/app.R
@@ -10,6 +10,28 @@ library(leaflet)
 library(htmltools)
 library(feather)
 
+
+
+plot.new()
+
+# read datafile
+city_coords <- read_feather("city_coordinates.feather")
+
+df1 <- read_feather("ams_rio_tokyo_hou_score.feather")
+
+# calculat mean scoring over all values
+df$mean_score  <- rowMeans(df[,c(6,7,8,10,11,12,13,14)])
+
+# create secuence based on spread of scoring values
+cut_values <- seq(min(df$mean_score)+0.1, max(df$mean_score)-0.1, length.out=5)
+cut_values_econ <- seq(min(df$Monetary)+1, max(df$Monetary)-1, length.out=5)
+
+
+
+df$name <- as.factor(df$name)
+
+df <- df1
+
 ## Only run this example in interactive R sessions
   # table example
 
