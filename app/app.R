6ddce5e4431ac3fe058a496f4b8edc9c5e089135
Chris <35135810+krakchris@users.noreply.github.com>
2018-04-12 09:38:56 +0200

    Revert "changed slider text"
    This reverts commit b18c9e263a8a3bfccb0824ce5a0faa9edc66ce90.

--- app/app.R
+++ app/app.R
@@ -106,7 +106,7 @@ ui <- bootstrapPage(
                     
                     tags$head(HTML("<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type = 'text/css'>")),
                     
-                    h1("What aspect of urban green space is most important to your city?", 
+                    h1("Use sliders to change importance of different score groups", 
                        style = "font-family: 'Roboto'; color: white; font-size: 20px; text-align: center;"),
                     
                     
