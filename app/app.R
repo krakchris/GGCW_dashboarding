a5c15449ad5b424b109b7502cc92124e177d54e3
Chris <35135810+krakchris@users.noreply.github.com>
2018-04-13 00:36:30 +0200

    remove stuff

--- app/app.R
+++ app/app.R
@@ -15,14 +15,7 @@ library(feather)
 # read datafile
 city_coords <- read_feather("city_coordinates.feather")
 
-df1 <- read_feather("ams_rio_tokyo_hou_score.feather")
 
-# calculat mean scoring over all values
-df$mean_score  <- rowMeans(df[,c(6,7,8,10,11,12,13,14)])
-
-# create secuence based on spread of scoring values
-cut_values <- seq(min(df$mean_score)+0.1, max(df$mean_score)-0.1, length.out=5)
-cut_values_econ <- seq(min(df$Monetary)+1, max(df$Monetary)-1, length.out=5)
 
 
 
