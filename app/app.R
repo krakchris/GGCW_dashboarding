946f124e2f784f069104409203b9f5391990cc14
Chris <35135810+krakchris@users.noreply.github.com>
2018-04-13 02:23:32 +0200

    changed df!!!!!

--- app/app.R
+++ app/app.R
@@ -160,8 +160,12 @@ ui <- shinyUI(fluidPage(  tags$head(tags$link(rel = "stylesheet", type = "text/c
                           
                           
                           
-                          column(1, align="center", tableOutput('social'), style = "font-family: 'Roboto'; color: white; font-size: 11px;  ",align="center"
-                          ) 
+                          column(1, align="center", tableOutput('eco'), style = "font-family: 'Roboto'; color: white; font-size: 11px;  ",align="center"
+                          ),
+                          column(1 ,offset = 1, align="center", tableOutput('social'), style = "font-family: 'Roboto'; color: white; font-size: 11px;  ",align="center"
+                          ),
+                          column(1 ,offset = 1, align="center", tableOutput('econ'),style = "font-family: 'Roboto'; color: white; font-size: 11px;  ",align="center"
+                          )  
                           
                           
                           
@@ -207,7 +211,6 @@ server <- function(input, output, session) {
   })
   
   
-
   observeEvent(input$map_marker_click, {
     ## Get the click info like had been doing
     
@@ -223,43 +226,33 @@ server <- function(input, output, session) {
       proxy %>% removeMarker(layerId="OSM_id")
     } else {
       
+      s_sc <- df[isolate(p$id) == df$OSM_id,]
+      
+      social = data.frame(c("Amenities","Gray vs Green ","Greenness in winter"), 
+                          c(s_sc$Soc_Amen,s_sc$Soc_Grey,s_sc$Soc_Winter))
+      
+      colnames(social) = c("Social Indicator", "Score")
       
       
       
+      eco = data.frame(c("Riparian buffer zone","Temp_Water","Impermeable surfaces","Stormwater Capture","Leaf Area Index value"), 
+                       c(s_sc$Infil_Rip,s_sc$Temp_Water,s_sc$Infil_Inper,s_sc$Infil_Storm,s_sc$Temp_LAI))
+      
+      colnames(eco) = c("Ecological Indicator", "Score")
       
       
       
+      econ = data.frame(c("Monetary Indicator"), 
+                        c(s_sc$Monetary))
       
-      
-      
-      
-      
-      
-      
-      # 
-      # 
-      # 
-      # 
-      # 
-      # eco = data.frame(c("Riparian buffer zone","Temp_Water","Impermeable surfaces","Stormwater Capture","Leaf Area Index value"), 
-      #                  c(s_sc$Infil_Rip,s_sc$Temp_Water,s_sc$Infil_Inper,s_sc$Infil_Storm,s_sc$Temp_LAI))
-      # 
-      # colnames(eco) = c("Indicator", "Score")
-      # 
-      # 
-      # 
-      # econ = data.frame(c("Monetary"), 
-      #                   c(s_sc$Monetary))
-      # 
-      # colnames(econ) = c("Monetary", "$")
+      colnames(econ) = c("Monetary", "$")
       
   
       
-      output$social <- renderTable(new[p$id == new$OSM_id,])
       
-      
-      # output$eco <- renderTable(eco)
-      # output$econ <- renderTable(econ)
+      output$social <- renderTable(social)
+      output$eco <- renderTable(eco)
+      output$econ <- renderTable(econ)
       
       
       
